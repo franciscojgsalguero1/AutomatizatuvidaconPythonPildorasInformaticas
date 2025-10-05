8a38f9da63451abcd5920d39f584b9081ccd2304
francisco <franciscojgsspam@gmail.com>
2025-10-05 21:46:21 +0200

    Video 8 Automatiza tu vida con Python. .bat y .sh. De Pildorasinformáticas (https://www.youtube.com/watch?v=cUhI2-kXet4&list=PLU8oAlHdN5BnNrwvZvOdu_FsUXHX200rA&index=8)

--- renombrar_archivos.py
+++ renombrar_archivos.py
@@ -1,4 +1,6 @@
 import os
+# si utilizas mac o linux
+#import stat
 
 carpeta="C:/Users/franc/Desktop/AutomatizatuvidaconPython"
 prefijo="imagen_"
@@ -10,9 +12,20 @@ for f in os.listdir(carpeta):
     if f.endswith(extensiones):
         archivos.append(f)
 
-for i, nombre_actual in enumerate(archivos, start=1):
-    extension_actual=os.path.splitext(nombre_actual)[1]
-    nuevo_nombre=f"{prefijo}{i:03}{extension_actual}"
-    ruta_actual=os.path.join(carpeta, nombre_actual)
-    ruta_nueva=os.path.join(carpeta, nuevo_nombre)
-    os.rename(ruta_actual, ruta_nueva)
+# Creación del archivo para deshacer renombrado (archivo tipo .bat y .sh)
+ruta_deshacer=os.path.join(carpeta, "deshacer.bat")
+#deshacer.sh
+with open(ruta_deshacer, "w", encoding="utf-8") as deshacer_mem:
+# ruta_deshacer.write("#!/bin/bash\n")
+# ruta_deshacer.write(f'cd "{carpeta}" || exis\n\n')
+    for i, nombre_actual in enumerate(archivos, start=1):
+        extension_actual=os.path.splitext(nombre_actual)[1]
+        nuevo_nombre=f"{prefijo}{i:03}{extension_actual}"
+        ruta_actual=os.path.join(carpeta, nombre_actual)
+        ruta_nueva=os.path.join(carpeta, nuevo_nombre)
+        os.rename(ruta_actual, ruta_nueva)
+        # deshacer_mem.write(f'mv "{nuevo_nombre}" "{nombre_actual}"\n')
+        deshacer_mem.write(f'rename "{nuevo_nombre}" "{nombre_actual}"\n')
+    # para que el archivo .bat desaparezca después de ejecutar el archivo con doble click
+    deshacer_mem.write("del \"%~f0\n")
+print(f"Renombrado completo. Ejecuta el .bat que hay en la carpeta para revertir cambios en '{ruta_deshacer}'")
